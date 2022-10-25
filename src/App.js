e0f4581745823c321415880ae3b4b71c5fb8a792
Ville Vartia <ville.v.vartia@utu.fi>
2022-10-25 14:39:51 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1>Sample Online Store</h1>
+      <h3>made by Ville Vartia</h3>
+      
+    </div>
+  );
+}
+
+export default App;
