9410ec2404cfaeb707077834a81d74478efbddd1
Ville Vartia <ville.v.vartia@utu.fi>
2022-10-26 18:01:12 +0300

    added a Card component base and implemented it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import CustomButton from './components/CustomBtn';
 import Navbar from './components/NavBar';
+import Card from './components/Card';
+
 function App() {
   return (
     
@@ -10,7 +12,15 @@ function App() {
       <h1>Sample Online Store.</h1>
       <h3>made by Ville Vartia</h3>
       <CustomButton txt = "proceed"/>
-      
+      <div>
+        <br/>
+      <Card 
+        className="section"
+        title="Image 1 shown here"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+      </div>
+     
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import StoreIcon from '@mui/icons-material/Store';
+export default function Card(props) {
+  return (
+    <div className={props.className}>
+        <div className="small-div">
+            <i className={props.className}></i>
+            <StoreIcon/> 
+        </div>
+        
+
+    <div className='big-div'>
+        <span className='div-title'>
+            {props.title}
+        </span>
+        <br/>
+        <span>
+            {props.description} 
+        </span>
+    </div>
+    </div>
+
+      
+  )
+}
