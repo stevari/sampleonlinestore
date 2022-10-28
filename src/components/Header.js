898a33b1db894d46373361bb973eb8e6c63e58e8
Ville Vartia <ville.v.vartia@utu.fi>
2022-10-28 15:16:13 +0300

    header component added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+ return (
+    <div>
+    <h2 style={{color:"#D1A55C", backgroundColor:"#201e1e", marginTop:10,marginBottom:0}}>Sample Online Store</h2>
+      <div style={{backgroundColor:"#201e1e"}}> made by Ville Vartia</div>
+    </div>
+    
+  )
+}
