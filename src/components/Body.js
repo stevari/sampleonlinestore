0cb2553537b998b6af996a13706e0bad841ec5fe
Ville Vartia <ville.v.vartia@utu.fi>
2022-11-03 18:44:32 +0200

    implemented new stockphotos to cards and customized styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import EuroOutlinedIcon from '@mui/icons-material/EuroOutlined';
+import beltandweight from "../img/beltandweight.jpg";
+import dumbbellsmetal from "../img/dumbbellsmetalsmall.jpg";
+import barbell from "../img/barbell.jpg";
 import CustomCard from './CustomCard';
 export default function Body() {
     return (
         <div>
             <div className='parent'>
-                <CustomCard title="Best price"
-                    img={`url(${EuroOutlinedIcon})`}
-                    text="We here at Sample Online Store offer you the best price on our products."
+                <CustomCard 
+                    img = {beltandweight}
+                    title="Best price"
+                    text="We here at Sample Online Store offer you the best price on our products. We belive in fair and sustainable pricing that serves you, the customer and us."
                 />
                 <CustomCard
+                    img = {dumbbellsmetal}
                     title="Best quality"
                     text="Our products are made from top-notch quality materials. We believe that quality defines who we are, so we only offer premium A+ tier equipment"
                 />
-                <CustomCard title="Best customer satisfaction"
+                <CustomCard
+                    img={barbell}
+                    title="Best customer satisfaction"
                     text="Our mission is to give you the best workout experience with quality equipment and products. According to surveys, our customer satisfaction rates are on average three times higher than our competitors."
                 />
 
--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -3,10 +3,10 @@ import Card from 'react-bootstrap/Card';
 function CustomCard(props) {
   return (
     <Card className='CustomCard' style={{borderRadius:25}}>
-      <Card.Img variant="top" src={props.img} />
+      <Card.Img variant="top" src={props.img} style={{borderRadius:"inherit",width:"100%"}}/>
       <Card.Body>
-        <Card.Title ><div style={{color:"#D1A55C"}}>{props.title}</div></Card.Title>
-        <Card.Text style={{marginBottom:"50px"}}>
+        <Card.Title className='CardTitle'>{props.title}</Card.Title>
+        <Card.Text className="CardText">
           {props.text}
         </Card.Text>
         
