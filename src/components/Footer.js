a1eec9a40258900c5cfac04e4520aed3789e8645
Ville Vartia <ville.v.vartia@utu.fi>
2022-11-08 09:47:36 +0200

    Proper footer init?

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='Footer' style={{marginLeft:-8,padding:1,marginRight:-8}}>
+      <h1>Gym Store</h1>
+        <h2 className='h2'>
+            This is the footer section of Gym Store.
+           
+        </h2>
+        <p>
+            You have reached the end of this page. 
+            and here you will find useless info
+            and links.
+            </p>
+        <h3 style={{backgroundColor:"#242020",textAlign:"center",padding:5,color:"#D1A55C",marginBottom:-10,marginLeft:"inherit"}}>
+            © 2022 copyright: Sampleonlinestore.com
+        </h3>
+    </div>
+  )
+}
